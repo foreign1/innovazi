e65987f4ac3fd8eca8d5be76f5a4d7d520ac066f
unknown <Jenyojohn24@gmail.com>
2020-06-23 21:16:30 +1245

    bump function for class

--- src/App.js
+++ src/App.js
@@ -1,34 +1,65 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+class App extends Component {
+  state = {
+    time: 0,
+  };
 
-  useEffect(() => {
-    fetch("/time")
+  componentDidMount() {
+    fetch("https://innovazi.herokuapp.com/time")
       .then((res) => res.json())
       .then((data) => {
-        setCurrentTime(data.time);
+        this.setState({ time: data.time });
       });
-  }, []);
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>The current time is {currentTime}.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <span>Today is {this.state.time}</span>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// class App extends Component {
+//   state = {
+//     time: [],
+//   };
+
+//   componentDidMount() {
+//     fetch("http://127.0.0.1:5000/time")
+//       .then((res) => res.json)
+//       .then((data) => {
+//         this.setState({ time: data });
+//       })
+//       .catch(console.log);
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>The current time is {this.state.time}.</p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+//export default App;
